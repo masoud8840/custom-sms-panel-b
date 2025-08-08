a6806cfb76ff70877ece4c91efde77ccb8a9b6f2
masoud <masoudgharedaghi@yahoo.com>
2025-08-08 11:49:59 +0330

    wrap into trycatch block

--- controllers/Message.js
+++ controllers/Message.js
@@ -103,41 +103,41 @@ module.exports.postNewMessage = async (req, res, next) => {
 };
 
 module.exports.postBulkSend = async (req, res, next) => {
-  const filePath = req.file.path;
-  const file = xlsx.readFile(filePath);
-  const sheetName = file.SheetNames[0];
-  const sheet = file.Sheets[sheetName];
-
-  const data = xlsx.utils.sheet_to_json(sheet);
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      console.log("ERROR in Message.js line 15");
-    }
-  });
-
-  const text = String(req.body.text);
-
-  const responses = {
-    messages: [],
-    cellphones: [],
-  };
-
-  const promise = data.map(async (d) => {
-    const cellphone = Object.values(d)[0];
-    const existingUser = await Member.findOne({ cellphone });
-
-    if (existingUser) {
-      const { responseText, cellphone } = replaceTextWithUserFields(
-        existingUser,
-        text
-      );
-      responses.cellphones.push(cellphone);
-      responses.messages.push(responseText);
-    }
-  });
-
-  await Promise.all(promise);
   try {
+    const filePath = req.file.path;
+    const file = xlsx.readFile(filePath);
+    const sheetName = file.SheetNames[0];
+    const sheet = file.Sheets[sheetName];
+
+    const data = xlsx.utils.sheet_to_json(sheet);
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.log("ERROR in Message.js line 15");
+      }
+    });
+
+    const text = String(req.body.text);
+
+    const responses = {
+      messages: [],
+      cellphones: [],
+    };
+
+    const promise = data.map(async (d) => {
+      const cellphone = Object.values(d)[0];
+      const existingUser = await Member.findOne({ cellphone });
+
+      if (existingUser) {
+        const { responseText, cellphone } = replaceTextWithUserFields(
+          existingUser,
+          text
+        );
+        responses.cellphones.push(cellphone);
+        responses.messages.push(responseText);
+      }
+    });
+
+    await Promise.all(promise);
     const sendBody = JSON.stringify({
       lineNumber: process.env.LINE_NUMBER,
       messageTexts: responses.messages,
@@ -166,115 +166,114 @@ module.exports.postBulkSend = async (req, res, next) => {
 };
 
 module.exports.getAllMessages = async (req, res, next) => {
-  const {
-    cellphone = "",
-    text = "",
-    from = "",
-    to = "",
-    page = 1,
-    limit = process.env.LIMIT,
-  } = req.query;
+  try {
+    const {
+      cellphone = "",
+      text = "",
+      from = "",
+      to = "",
+      page = 1,
+      limit = process.env.LIMIT,
+    } = req.query;
 
-  const filters = {};
-  if (cellphone) filters.from = { $regex: cellphone, $options: "i" };
-  if (text) {
-    filters.text = text;
-  }
-  if (from || to) {
-    filters.createdAt = {};
-    if (from) filters.createdAt.$gte = from;
-    if (to) filters.createdAt.$lte = to;
-  }
+    const filters = {};
+    if (cellphone) filters.from = { $regex: cellphone, $options: "i" };
+    if (text) {
+      filters.text = text;
+    }
+    if (from || to) {
+      filters.createdAt = {};
+      if (from) filters.createdAt.$gte = from;
+      if (to) filters.createdAt.$lte = to;
+    }
 
-  const pageNumber = parseInt(page, 10);
-  const pageLimit = parseInt(limit, 10);
-  const skip = (pageNumber - 1) * pageLimit;
+    const pageNumber = parseInt(page, 10);
+    const pageLimit = parseInt(limit, 10);
+    const skip = (pageNumber - 1) * pageLimit;
 
-  // TODO:
-  const data = await Message.find(filters)
-    .populate({
-      path: "from_id",
-      model: "Member",
-    })
-    .lean()
-    .limit(pageLimit)
-    .skip(skip)
-    .sort({ createdAt: -1 });
+    // TODO:
+    const data = await Message.find(filters)
+      .populate({
+        path: "from_id",
+        model: "Member",
+      })
+      .lean()
+      .limit(pageLimit)
+      .skip(skip)
+      .sort({ createdAt: -1 });
 
-  const total = await Message.countDocuments(filters);
-  return res.status(200).json({
-    message: {},
-    data,
-    pagination: {
-      total,
-      page: pageNumber,
-      totalPages: Math.ceil(total / pageLimit),
-      limit: pageLimit,
-    },
-  });
+    const total = await Message.countDocuments(filters);
+    return res.status(200).json({
+      message: {},
+      data,
+      pagination: {
+        total,
+        page: pageNumber,
+        totalPages: Math.ceil(total / pageLimit),
+        limit: pageLimit,
+      },
+    });
+  } catch (error) {}
 };
 
 module.exports.downloadAllMessages = async (req, res, next) => {
-  const {
-    cellphone = "",
-    text = "",
-    from = "",
-    to = "",
-    page = 1,
-    limit = process.env.LIMIT,
-  } = req.query;
+  try {
+    const { cellphone = "", text = "", from = "", to = "" } = req.query;
 
-  const filters = {};
-  if (cellphone) filters.from = { $regex: cellphone, $options: "i" };
-  if (text) {
-    filters.text = text;
+    const filters = {};
+    if (cellphone) filters.from = { $regex: cellphone, $options: "i" };
+    if (text) {
+      filters.text = text;
+    }
+    if (from || to) {
+      filters.createdAt = {};
+      if (from) filters.createdAt.$gte = from;
+      if (to) filters.createdAt.$lte = to;
+    }
+
+    const data = await Message.find(filters).lean().populate({
+      path: "from_id",
+      model: "Member",
+    });
+    const newData = data.map((d) => {
+      const newD = {
+        _id: d._id,
+        نام: d.from_id?.fname ?? "-",
+        "نام خانوادگی": d.from_id?.lname ?? "-",
+        "کد شناسایی": d.from_id?.personalCode ?? "-",
+        "کد ملی": d.from_id?.nationalityCode ?? "-",
+        "تلفن همراه": d.from ?? "-",
+        "متن پیامک": d.text ?? "-",
+        "تاریخ تولد": d.from_id?.birthdate ?? "-",
+        جنسیت: d.from_id?.gender ?? "-",
+        نسبت: d.from_id?.relationship ?? "-",
+        "زمان ارسال": formatMessageSendTime(d.createdAt) ?? "-",
+      };
+
+      return newD;
+    });
+
+    const workbook = xlsx.utils.book_new();
+    const worksheet = xlsx.utils.json_to_sheet(newData);
+
+    xlsx.utils.book_append_sheet(workbook, worksheet, "Messages");
+
+    const excelBuffer = xlsx.write(workbook, {
+      bookType: "xlsx",
+      type: "buffer",
+    });
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="messages_export.xlsx"'
+    );
+    res.send(excelBuffer);
+  } catch (error) {
+    return res.status(400).json({ message: {} });
   }
-  if (from || to) {
-    filters.createdAt = {};
-    if (from) filters.createdAt.$gte = from;
-    if (to) filters.createdAt.$lte = to;
-  }
-  const pageNumber = parseInt(page, 10);
-  const pageLimit = parseInt(limit, 10);
-  const skip = (pageNumber - 1) * pageLimit;
-
-  const data = await Message.find(filters).lean().limit(pageLimit).skip(skip);
-  const newData = data.map((d) => {
-    const newD = {
-      _id: d._id,
-      نام: d.from_id?.fname ?? "-",
-      "نام خانوادگی": d.from_id?.lname ?? "-",
-      "کد شناسایی": d.from_id?.personalCode ?? "-",
-      "کد ملی": d.from_id?.nationalityCode ?? "-",
-      "تلفن همراه": d.from ?? "-",
-      "متن پیامک": d.text ?? "-",
-      "تاریخ تولد": d.from_id?.birthdate ?? "-",
-      جنسیت: d.from_id?.gender ?? "-",
-      نسبت: d.from_id?.relationship ?? "-",
-      "زمان ارسال": formatMessageSendTime(d.createdAt) ?? "-",
-    };
-
-    return newD;
-  });
-
-  const workbook = xlsx.utils.book_new();
-  const worksheet = xlsx.utils.json_to_sheet(newData);
-
-  xlsx.utils.book_append_sheet(workbook, worksheet, "Messages");
-
-  const excelBuffer = xlsx.write(workbook, {
-    bookType: "xlsx",
-    type: "buffer",
-  });
-  res.setHeader(
-    "Content-Type",
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  );
-  res.setHeader(
-    "Content-Disposition",
-    'attachment; filename="messages_export.xlsx"'
-  );
-  res.send(excelBuffer);
 };
 
 module.exports.postResetMessages = async (req, res, next) => {
